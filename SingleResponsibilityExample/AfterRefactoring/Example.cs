1cc7c883cccfd2a9144a6cf41d918cc98e06334e
thebluecode <thebluecode.channel@gmail.com>
2017-06-13 11:27:49 -0300

    Upload Single Responsibility Example

---
+++ SingleResponsibilityExample/AfterRefactoring/Example.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AfterRefactoring
+{
+    public class Product
+    {
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public Product(string name, int price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public void SaveProductToDatabase()
+        {
+            Console.WriteLine("Saving Product: {{ Name: '{0}', Price: {1} }}", this.Name, this.Price);
+        }
+    }
+
+    public class Mailer
+    {
+        public void Email(Product product)
+        {
+            Console.WriteLine("Emailing Product: {{ Name: '{0}', Price: {1} }}", product.Name, product.Price);
+        }
+    }
+}
